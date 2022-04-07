72f773c5ef7c5eb373563a5759ccac5dd820aed7
tarek-morshed <tarek.morshid@gmail.com>
2022-04-07 15:42:11 +0200

    Fix settings tables updates

--- services/settings.services.js
+++ services/settings.services.js
@@ -3,23 +3,25 @@ const Setting = require('../models/setting');
 class SettingsServices {
 
     // get current settings
-    static async getSettings(id){
+    static async getSettings(id) {
         return Setting.query().findById(id);
     }
 
     // update azan setting
-    static async setSettings(id, payload){
+    static async setSettings(id, payload) {
         await Setting.query().findById(id).patch(payload);
     }
 
     // get settings for a selected prayer time by name
-    static async getPrayerTimeSettings(name){
+    static async getPrayerTimeSettings(name) {
         return Setting.query()
             .findOne({prayer_time: name})
-            .select('settings.z1', 'settings.z2', 'settings.z3', 'settings.z4', 'settings.z5', 'settings.z6', 'settings.z7', 'is_enabled' )
+            .select('settings.z1_status',
+                'settings.z2_status', 'settings.z3_status', 'settings.z4_status',
+                'settings.z5_status', 'settings.z6_status', 'settings.z7_status', 'is_enabled')
     }
 
-    static async enableDisablePrayerTimeSound(id, value){
+    static async enableDisablePrayerTimeSound(id, value) {
         await Setting.query().findById(id).patch({is_enabled: value});
     }
 }
