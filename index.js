9fce2edbebae88615aa5945e2737dced6f3b41fd
tarek-morshed <tarek.morshid@gmail.com>
2022-04-10 00:53:04 +0200

    Solve Maghrib Bug

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const zoneRouter = require('./routes/zones.route');
-const settingsRouter = require('./routes/settings.route');
-const prayerTimesRouter = require('./routes/prayer.router');
-const ZoneServices = require('./services/zone.services');
-require('./utils/serialCom').init();
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const zoneRouter = require("./routes/zones.route");
+const settingsRouter = require("./routes/settings.route");
+const prayerTimesRouter = require("./routes/prayer.router");
+const ZoneServices = require("./services/zone.services");
+require("./utils/serialCom").init();
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
-app.use('/api/zones', zoneRouter);
-app.use('/api/settings', settingsRouter);
-app.use('/api/prayertimes', prayerTimesRouter);
+app.use(morgan("tiny"));
+app.use("/api/zones", zoneRouter);
+app.use("/api/settings", settingsRouter);
+app.use("/api/prayertimes", prayerTimesRouter);
 
 const port = process.env.PORT;
 
-app.listen(port, async ()=>{
-    console.log(`Server is running on port ${port}`);
-    // initialize prayer time with start up
-    await require('./services/azan.services').fetchPrayerTimes();
-    await ZoneServices.controlAll(0)
-    require('./cronJobs/cron')
-
+app.listen(port, async () => {
+  console.log(`Server is running on port ${port}`);
+  // initialize prayer time with start up
+  await require("./services/azan.services").fetchPrayerTimes();
+  await ZoneServices.controlAll("0");
+  require("./cronJobs/cron");
 });
